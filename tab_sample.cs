466d0874fbc285e29edd24f7a45b850c9e5e6919
Yusuke Tarumi <y.tarumi00@gmail.com>
2013-09-04 15:15:51 +0900

    edit tab_sample.cs
    finish make_3button

--- tab_sample.cs
+++ tab_sample.cs
@@ -6,10 +6,10 @@ using System.IO;
 public class MyButton : Button{
 	public int tab_num;
 	public int val;
-  public MyButton(int t,int v){
+	public MyButton(int t,int v){
 		set_tab_num(t);
 		set_val(v);
-  }
+	}
 	void set_tab_num(int t){
 		this.tab_num = t;
 	}
@@ -29,13 +29,13 @@ public class Form1 : System.Windows.Forms.Form
 	private System.ComponentModel.Container components;
 
 	// Declare variables.
-	private System.Windows.Forms.RadioButton tab3RadioButton2;
-	private System.Windows.Forms.RadioButton tab3RadioButton1;
-	private System.Windows.Forms.CheckBox tab2CheckBox3;
-	private System.Windows.Forms.CheckBox tab2CheckBox2;
-	private System.Windows.Forms.CheckBox tab2CheckBox1;
-	private System.Windows.Forms.Label tab1Label1;
-//  private System.Windows.Forms.Button tab1Button1;
+	//  private System.Windows.Forms.RadioButton tab3RadioButton2;
+	//  private System.Windows.Forms.RadioButton tab3RadioButton1;
+	//  private System.Windows.Forms.CheckBox tab2CheckBox3;
+	//  private System.Windows.Forms.CheckBox tab2CheckBox2;
+	//  private System.Windows.Forms.CheckBox tab2CheckBox1;
+	//  private System.Windows.Forms.Label tab1Label1;
+	//  private System.Windows.Forms.Button tab1Button1;
 	private MyButton[] tab1Button;
 	private MyButton[] tab2Button;
 	private MyButton[] tab3Button;
@@ -55,20 +55,20 @@ public class Form1 : System.Windows.Forms.Form
 	private void InitializeComponent(Size W_size)
 	{
 		this.components = new System.ComponentModel.Container();
-		this.tabPage1 = new System.Windows.Forms.TabPage();
-		this.tab2CheckBox3 = new System.Windows.Forms.CheckBox();
-		this.tab3RadioButton2 = new System.Windows.Forms.RadioButton();
 		this.tabControl1 = new System.Windows.Forms.TabControl();
-		this.tab2CheckBox2 = new System.Windows.Forms.CheckBox();
-		this.tab2CheckBox1 = new System.Windows.Forms.CheckBox();
-		this.tab3RadioButton1 = new System.Windows.Forms.RadioButton();
-		this.tab1Label1 = new System.Windows.Forms.Label();
-		this.tabPage3 = new System.Windows.Forms.TabPage();
+		this.tabPage1 = new System.Windows.Forms.TabPage();
 		this.tabPage2 = new System.Windows.Forms.TabPage();
-//    this.tab1Button1 = new System.Windows.Forms.Button();
+		this.tabPage3 = new System.Windows.Forms.TabPage();
 		this.tab1Button = new MyButton[3];
-//    this.tab2Button = new MyButton[3];
-//    this.tab3Button = new MyButton[3];
+		this.tab2Button = new MyButton[3];
+		this.tab3Button = new MyButton[3];
+		//    this.tab2CheckBox3 = new System.Windows.Forms.CheckBox();
+		//    this.tab3RadioButton2 = new System.Windows.Forms.RadioButton();
+		//    this.tab2CheckBox2 = new System.Windows.Forms.CheckBox();
+		//    this.tab2CheckBox1 = new System.Windows.Forms.CheckBox();
+		//    this.tab3RadioButton1 = new System.Windows.Forms.RadioButton();
+		//    this.tab1Label1 = new System.Windows.Forms.Label();
+		//    this.tab1Button1 = new System.Windows.Forms.Button();
 
 		int allowance = 20;
 		Size T_size = new System.Drawing.Size(640,360);
@@ -86,61 +86,88 @@ public class Form1 : System.Windows.Forms.Form
 		tabPage3.Text = "mouse";
 		tabPage3.Size = T_size2;
 		tabPage3.TabIndex = 2;
+
 		for(int i = 0;i < 3;i++){
 			string s = i.ToString();
 			tab1Button[i] = new MyButton(0,i){
 				Text = "cheek"+s,
-					Location = new System.Drawing.Point(10,50+i*50),
-					Size = new System.Drawing.Size(80,40),
-					TabIndex = i,
+						 Location = new System.Drawing.Point(10,50+i*50),
+						 Size = new System.Drawing.Size(80,40),
+						 TabIndex = i,
 			};
 			tab1Button[i].Click += new System.EventHandler(this.tab1Button_Click);
 		}
-		tab2CheckBox3.Location = new System.Drawing.Point(32, 136);
-		tab2CheckBox3.Text = "checkBox3";
-		tab2CheckBox3.Size = new System.Drawing.Size(176, 32);
-		tab2CheckBox3.TabIndex = 2;
-		tab2CheckBox3.Visible = true;
-		tab3RadioButton2.Location = new System.Drawing.Point(40, 72);
-		tab3RadioButton2.Text = "radioButton2";
-		tab3RadioButton2.Size = new System.Drawing.Size(152, 24);
-		tab3RadioButton2.TabIndex = 1;
-		tab3RadioButton2.Visible = true;
-		tab2CheckBox2.Location = new System.Drawing.Point(32, 80);
-		tab2CheckBox2.Text = "checkBox2";
-		tab2CheckBox2.Size = new System.Drawing.Size(176, 32);
-		tab2CheckBox2.TabIndex = 1;
-		tab2CheckBox2.Visible = true;
-		tab2CheckBox1.Location = new System.Drawing.Point(32, 24);
-		tab2CheckBox1.Text = "checkBox1";
-		tab2CheckBox1.Size = new System.Drawing.Size(176, 32);
-		tab2CheckBox1.TabIndex = 0;
-		tab3RadioButton1.Location = new System.Drawing.Point(40, 32);
-		tab3RadioButton1.Text = "radioButton1";
-		tab3RadioButton1.Size = new System.Drawing.Size(152, 24);
-		tab3RadioButton1.TabIndex = 0;
-//    tab1Button1.Location = new System.Drawing.Point(88, 144);
-//    tab1Button1.Size = new System.Drawing.Size(80, 40);
-//    tab1Button1.TabIndex = 0;
-//    tab1Button1.Text = "button1";
-//    tab1Button.Click += new 
-//      System.EventHandler(this.tab1Button_Click);
+		for(int i = 0;i < 3;i++){
+			string s = i.ToString();
+			tab2Button[i] = new MyButton(1,i){
+				Text = "nose"+s,
+						 Location = new System.Drawing.Point(10,50+i*50),
+						 Size = new System.Drawing.Size(80,40),
+						 TabIndex = i,
+			};
+			tab2Button[i].Click += new System.EventHandler(this.tab2Button_Click);
+		}
+		for(int i = 0;i < 3;i++){
+			string s = i.ToString();
+			tab3Button[i] = new MyButton(2,i){
+				Text = "mouse"+s,
+						 Location = new System.Drawing.Point(10,50+i*50),
+						 Size = new System.Drawing.Size(80,40),
+						 TabIndex = i,
+			};
+			tab3Button[i].Click += new System.EventHandler(this.tab3Button_Click);
+		}
+
+		//    tab2CheckBox3.Location = new System.Drawing.Point(32, 136);
+		//    tab2CheckBox3.Text = "checkBox3";
+		//    tab2CheckBox3.Size = new System.Drawing.Size(176, 32);
+		//    tab2CheckBox3.TabIndex = 2;
+		//    tab2CheckBox3.Visible = true;
+		//    tab3RadioButton2.Location = new System.Drawing.Point(40, 72);
+		//    tab3RadioButton2.Text = "radioButton2";
+		//    tab3RadioButton2.Size = new System.Drawing.Size(152, 24);
+		//    tab3RadioButton2.TabIndex = 1;
+		//    tab3RadioButton2.Visible = true;
+		//    tab2CheckBox2.Location = new System.Drawing.Point(32, 80);
+		//    tab2CheckBox2.Text = "checkBox2";
+		//    tab2CheckBox2.Size = new System.Drawing.Size(176, 32);
+		//    tab2CheckBox2.TabIndex = 1;
+		//    tab2CheckBox2.Visible = true;
+		//    tab2CheckBox1.Location = new System.Drawing.Point(32, 24);
+		//    tab2CheckBox1.Text = "checkBox1";
+		//    tab2CheckBox1.Size = new System.Drawing.Size(176, 32);
+		//    tab2CheckBox1.TabIndex = 0;
+		//    tab3RadioButton1.Location = new System.Drawing.Point(40, 32);
+		//    tab3RadioButton1.Text = "radioButton1";
+		//    tab3RadioButton1.Size = new System.Drawing.Size(152, 24);
+		//    tab3RadioButton1.TabIndex = 0;
+		//    tab1Button1.Location = new System.Drawing.Point(88, 144);
+		//    tab1Button1.Size = new System.Drawing.Size(80, 40);
+		//    tab1Button1.TabIndex = 0;
+		//    tab1Button1.Text = "button1";
+		//    tab1Button.Click += new 
+		//      System.EventHandler(this.tab1Button_Click);
 		this.Text = "Form1";
 
 		// Adds controls to the second tab page.
-		tabPage2.Controls.Add(this.tab2CheckBox3);
-		tabPage2.Controls.Add(this.tab2CheckBox2);
-		tabPage2.Controls.Add(this.tab2CheckBox1);
+		//    tabPage2.Controls.Add(this.tab2CheckBox3);
+		//    tabPage2.Controls.Add(this.tab2CheckBox2);
+		//    tabPage2.Controls.Add(this.tab2CheckBox1);
 		// Adds controls to the third tab page.
-		tabPage3.Controls.Add(this.tab3RadioButton2);
-		tabPage3.Controls.Add(this.tab3RadioButton1);
+		//    tabPage3.Controls.Add(this.tab3RadioButton2);
+		//    tabPage3.Controls.Add(this.tab3RadioButton1);
 		// Adds controls to the first tab page.
-		tabPage1.Controls.Add(this.tab1Label1);
-//    tabPage1.Controls.Add(this.tab1Button1);
+		//    tabPage1.Controls.Add(this.tab1Label1);
+		//    tabPage1.Controls.Add(this.tab1Button1);
 		for(int i = 0;i < 3;i++)
 			tabPage1.Controls.Add(this.tab1Button[i]);
+		for(int i = 0;i < 3;i++)
+			tabPage2.Controls.Add(this.tab2Button[i]);
+		for(int i = 0;i < 3;i++)
+			tabPage3.Controls.Add(this.tab3Button[i]);
 		// Adds the TabControl to the form.
 		this.Controls.Add(this.tabControl1);
+
 		// Adds the tab pages to the TabControl.
 		tabControl1.Controls.Add(this.tabPage1);
 		tabControl1.Controls.Add(this.tabPage2);
@@ -148,6 +175,14 @@ public class Form1 : System.Windows.Forms.Form
 	}
 
 	private void tab1Button_Click (object sender, System.EventArgs e)
+	{
+		// Inserts the code that should run when the button is clicked.
+	}
+	private void tab2Button_Click (object sender, System.EventArgs e)
+	{
+		// Inserts the code that should run when the button is clicked.
+	}
+	private void tab3Button_Click (object sender, System.EventArgs e)
 	{
 		// Inserts the code that should run when the button is clicked.
 	}
@@ -159,4 +194,3 @@ class Program{
 		Application.Run(new Form1());
 	}
 }
-
