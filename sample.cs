304d452f1a1e33c682271c85a674055686f50e17
Yusuke Tarumi <y.tarumi00@gmail.com>
2013-09-04 10:28:00 +0900

    add tab_sample.cs

--- sample.cs
+++ sample.cs
@@ -4,6 +4,9 @@ using System.Windows.Forms;
 using System.Text;
 using System.IO;
 
+public class MyButton : Button{
+	public int val = 0 ;
+}
 class Program
 {
 	[STAThread]
@@ -16,7 +19,8 @@ class Program
 
 class Form1 : Form
 {
-	Button[] button = new Button[3];  // ボタン
+	MyButton[] button = new MyButton[3];  // ボタン
+	int Rout ;
 
 	public Form1()
 	{
@@ -27,7 +31,7 @@ class Form1 : Form
 //    int B_width = 50;
 //    int B_height = 30;
 		this.ClientSize = new System.Drawing.Size(W_size.Width,W_size.Height);
-		button[0] = new Button()
+		button[0] = new MyButton()
 		{
 			Text = "ボタン A",
 					 TabIndex = 0,  // フォーカスの移る順位 0 (最優先)
@@ -35,10 +39,11 @@ class Form1 : Form
 					 Size = new Size(B_size.Width,B_size.Height),
 					 UseVisualStyleBackColor = true,  // ビジュアルスタイル
 					 Cursor = Cursors.Hand,  // 手形カーソル
+					 val = 0,
 		};
 		button[0].Click += new EventHandler(button_Click);
 
-		button[1] = new Button()
+		button[1] = new MyButton()
 		{
 			Text = "ボタン B",
 					 TabIndex = 1,
@@ -46,10 +51,11 @@ class Form1 : Form
 					 Size = new Size(B_size.Width,B_size.Height),
 					 UseVisualStyleBackColor = true,
 					 Cursor = Cursors.Hand,  // 手形カーソル
+					 val = 1,
 		};
 		button[1].Click += new EventHandler(button_Click);
 
-		button[2] = new Button()
+		button[2] = new MyButton()
 		{
 			Text = "ボタン C",
 					 TabIndex = 2,
@@ -57,6 +63,7 @@ class Form1 : Form
 					 Size = new Size(B_size.Width,B_size.Height),
 					 UseVisualStyleBackColor = true,
 					 Cursor = Cursors.Hand,  // 手形カーソル
+					 val = 2,
 		};
 		button[2].Click += new EventHandler(button_Click);
 
@@ -68,6 +75,7 @@ class Form1 : Form
 	void button_Click(object sender, EventArgs e)
 	{
 		// 押されたボタンのテキストをタイトルバーに表示
-		this.Text = (sender as Button).Text;
+		this.Rout = (sender as MyButton).val;
+		Console.Write(this.Rout);
 	}
 }
